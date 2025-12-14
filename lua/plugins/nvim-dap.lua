1203305d99de427c247e555769b59aa19814ad43
Artem Stasyuk <stasyukartem2@gmail.com>
2025-12-14 22:47:19 +0100

    DAP added plugins

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,20 +1,50 @@
--- still need to configure, need for building and debugging
 return {
-  {
+{
     "mfussenegger/nvim-dap",
-    config = function() require("dap") end,
-  },
+    event = "VeryLazy",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "jay-babu/mason-nvim-dap.nvim",
+        "theHamsta/nvim-dap-virtual-text",
+    },
+},
+
+
+  -- DAP UI
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio" },
+    dependencies = { "mfussenegger/nvim-dap" },
     config = function()
-      require("dapui").setup()
-      -- Open/close automatically
       local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+
+      -- Auto open/close DAP UI
       dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
       dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
       dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
     end,
   },
+
+  -- Mason for DAP
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = { "mfussenegger/nvim-dap", "williamboman/mason.nvim" , "nvim-neotest/nvim-nio"},
+    config = function()
+      local ok, mason_dap = pcall(require, "mason-nvim-dap")
+      if ok then
+        mason_dap.setup({
+          ensure_installed = { "codelldb " },
+          automatic_installation = true,
+        })
+      end
+    end,
+  },
+
+  -- Mason core (UI + installer)
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
 }
+
