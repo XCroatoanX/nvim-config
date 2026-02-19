3d2d8690b48b1343bf44821e091f6df30932c4c2
XCroatoanX <stasyukartem2@gmail.com>
2026-02-19 21:47:09 +0100

    Updated telescope to cmake

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+     ,{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install' }
       "tsakirist/telescope-lazy.nvim",
     },
     config = function()
