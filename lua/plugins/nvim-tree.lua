3b9070f7220d18d963be4761d6c82aa6016c45dc
Artem Stasyuk <arst@netcompany.cloud>
2026-06-26 14:07:53 +0200

    feat(nvim-tree): add indents

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,9 @@
 vim.pack.add({ "https://github.com/nvim-tree/nvim-tree.lua" })
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    renderer = {
+        indent_markers = {
+            enable = true,
+        },
+    },
+})
 vim.keymap.set("n", "<C-e>", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle file explorer" })
